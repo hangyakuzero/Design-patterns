80089c44a0d4bc83c34cbdcfb6b200d66fbe7e4c
Hangyaku <hangyakuzero@gmail.com>
2024-12-25 23:30:14 +0530

    observer pattern is really interesting

--- observer.java
+++ observer.java
@@ -1,9 +1,39 @@
 import java.util.List;
 public interface Subject{
 public void notify();
-public void addobserver();
-  public void removeobserver();
+public void addobserver(Observer observer);
+public void removeobserver(Observer observer);
 }
 public interface Observer{
-public void update();
+public void update(float temp, float humidity, float pressure);
+}
+public class WeatherStation implements Subject{
+  private List<Observer> observers;
+  private float temp;
+  private float humidity;
+  private float pressure;
+
+  public WeatherStation(){
+    this.observers = new ArrayList<>();
+  }
+  public void addobserver(Observer Observer){
+    observers.add(observer);
+  }
+  public void removeobserver{
+    observers.remove(Observer);
+  }
+  public void notify(){
+    for(Observer observer: observers){
+      observer.update(temp,humidity,pressure);
+    }
+  }
+  public void setdata(float temo, float humidity, float pressure){
+    this.temp = temp;
+    this.humidity = humidity;
+    this.pressure = pressure;
+    changed();
+  }
+  public void changed(){
+    notify();
+  }
 }
